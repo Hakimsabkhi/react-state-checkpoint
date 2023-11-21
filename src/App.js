c55a4725ebcd3fa6c3e5f72048c84d0b56069e75
Hakimsabkhi <62035010+Hakimsabkhi@users.noreply.github.com>
2023-11-21 15:39:42 +0100

    11/21/23

--- src/App.js
+++ src/App.js
@@ -1,54 +1,55 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    state = {
-        person: {
-            fullName: "John Doe",
-            bio: "Software Developer",
-            imgSrc: "path_to_image.jpg",
-            profession: "Engineer"
-        },
-        shows: false,
-        timeElapsed: 0
-    };
+  state = {
+    person: {
+      fullName: 'John Doe',
+      bio: 'Software Developer',
+      imgSrc:
+        'https://icons.veryicon.com/png/o/miscellaneous/practical-life-icon/man-22.png',
+      profession: 'Engineer',
+    },
+    shows: false,
+    timeElapsed: 0,
+  };
 
-    componentDidMount() {
-        this.mountTime = Date.now();
-        this.timerID = setInterval(() => {
-            this.setState({ timeElapsed: Date.now() - this.mountTime });
-        }, 1000);
-    }
+  componentDidMount() {
+    this.mountTime = Date.now();
+    this.timerID = setInterval(() => {
+      this.setState({ timeElapsed: Date.now() - this.mountTime });
+    }, 1000);
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-    toggleShow = () => {
-        this.setState(prevState => ({ shows: !prevState.shows }));
-    };
+  toggleShow = () => {
+    this.setState((prevState) => ({ shows: !prevState.shows }));
+  };
 
-    renderProfile() {
-        const { fullName, bio, imgSrc, profession } = this.state.person;
-        return (
-            <div>
-                <h1>{fullName}</h1>
-                <p>{bio}</p>
-                <img src={imgSrc} alt="Profile" />
-                <p>{profession}</p>
-            </div>
-        );
-    }
+  renderProfile() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    return (
+      <div>
+        <h1>{fullName}</h1>
+        <p>{bio}</p>
+        <img src={imgSrc} alt='Profile' />
+        <p>{profession}</p>
+      </div>
+    );
+  }
 
-    render() {
-        const { shows, timeElapsed } = this.state;
-        return (
-            <div className="App">
-                <button onClick={this.toggleShow}>Toggle Profile</button>
-                {shows && this.renderProfile()}
-                <p>Time since mounted: {Math.floor(timeElapsed / 1000)} seconds</p>
-            </div>
-        );
-    }
+  render() {
+    const { shows, timeElapsed } = this.state;
+    return (
+      <div className='App'>
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        {shows && this.renderProfile()}
+        <p>Time since mounted: {Math.floor(timeElapsed / 1000)} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
