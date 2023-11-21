050ed799cb84e90b20ce7ca4747a8206e6498393
Hakimsabkhi <62035010+Hakimsabkhi@users.noreply.github.com>
2023-11-21 15:27:32 +0100

    11/21/23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        person: {
+            fullName: "John Doe",
+            bio: "Software Developer",
+            imgSrc: "path_to_image.jpg",
+            profession: "Engineer"
+        },
+        shows: false,
+        timeElapsed: 0
+    };
+
+    componentDidMount() {
+        this.mountTime = Date.now();
+        this.timerID = setInterval(() => {
+            this.setState({ timeElapsed: Date.now() - this.mountTime });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    toggleShow = () => {
+        this.setState(prevState => ({ shows: !prevState.shows }));
+    };
+
+    renderProfile() {
+        const { fullName, bio, imgSrc, profession } = this.state.person;
+        return (
+            <div>
+                <h1>{fullName}</h1>
+                <p>{bio}</p>
+                <img src={imgSrc} alt="Profile" />
+                <p>{profession}</p>
+            </div>
+        );
+    }
+
+    render() {
+        const { shows, timeElapsed } = this.state;
+        return (
+            <div className="App">
+                <button onClick={this.toggleShow}>Toggle Profile</button>
+                {shows && this.renderProfile()}
+                <p>Time since mounted: {Math.floor(timeElapsed / 1000)} seconds</p>
+            </div>
+        );
+    }
 }
 
 export default App;
